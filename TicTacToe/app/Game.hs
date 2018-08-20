df94d425dc7549340c1747ab42ebbb5aec7e3abf
Sgusten <sara.gustavsson94@live.se>
2018-08-20 07:55:59 +0200

    Done game

--- TicTacToe/app/Game.hs
+++ TicTacToe/app/Game.hs
@@ -1,31 +1,55 @@
+{-
+   Project: Tic-Tac-Toe
+   Module: Game.hs
+   Authors: Sara Gustavsson and William Sandkvist
+   2018-08-18
+-}
+
 module Game where
 
-
 import Data.List
+import Test.HUnit
 
-data Stone = O | X | N
+
+{- Represents the different players stones.
+ -}
+data Stone = O | X
    deriving (Show, Eq)
+
+{- Represents the human player and the AI player.
+ -}
 data Player = Player | AI
    deriving (Show, Eq)
 
+{- Represents a square on the game board that is not empty. Stone shows which player is on the square and the integers represents
+   the x- and y-coordinates, or columnn and row, on the board.
+ -}
 data Square = Position Stone Integer Integer
    deriving (Show, Eq)
+
 type Board = [Square]
 
 
--- Prints the curent board
+{- showBoard board
+   Prints the board in the console, which is 10x10 squares with letters A - I at the top and numbers 1-9 on the left.
+   SIDE EFFECTS: Prints the board to the console.
+-}
 showBoard :: Board -> IO()
 
 showBoard board = printFirstRow >> (printRows board 0 1)
 
 
--- Prints each row of the board
+{- printRows xs x' y'
+   Prints the game board row by row, from left to right.
+   SIDE EFFECTS: Prints the board to the console.
+-}
 printRows :: Board -> Integer -> Integer -> IO()
+
+-- VARIANT: length xs
 printRows ((Position s x y):xs) x' y'
    | y' == 10 = putStrLn ""
    | x' == 0 = putStr "| " >> printNumber y' >> putStr " | " >> printRows ((Position s x y):xs) (x' + 1) y'
    | x' == 10 = putStrLn "" >> printRows ((Position s x y):xs) 0 (y' + 1)
-   | x' == 0 = putStrLn "------------"
    | x == x' && y == y' = printStone s >> putStr " | " >> printRows xs (x' + 1) y'
    |otherwise = putStr "  | " >> printRows ((Position s x y):xs) (x' + 1) y'
 printRows [] x' y'
@@ -35,13 +59,20 @@ printRows [] x' y'
    |otherwise = putStr "  | " >> printRows [] (x' + 1) y'
 
 
--- Prints the first row
+{- printFirstRow
+   Prints the first row, containing the letters A - I, to the console.
+   SIDE EFFECTS: Prints "|   | A | B | C | D | E | F | G | H | I |" to the console
+-}
 printFirstRow :: IO()
 
 printFirstRow = putStrLn "|   | A | B | C | D | E | F | G | H | I |"
 
 
--- If you use print on a variable then putStrLn is used. This makes it so a nl is not created
+{- printNumber n
+   Converts an integer in to a string and prints it to the console. This is to avoind the new line created by using the print command.
+   PRE:  The integer needs to be a number 1 - 9.
+   SIDE EFFECTS: Prints the given integer to the screen.
+-}
 printNumber :: Integer -> IO()
 
 printNumber n
@@ -56,7 +87,10 @@ printNumber n
    | n == 9 = putStr "9"
 
 
---Same as the one above
+{- printStone s
+   Converts a stone in to a string and prints it to the console. This is to avoid the new line created by using the print command.
+   SIDE EFFECTS: Prints the given integer to the screen.
+-}
 printStone :: Stone -> IO()
 
 printStone s
@@ -64,16 +98,25 @@ printStone s
    | s == X = putStr "X"
 
 
--- Adds a move to the appropriet place in the Board list
+{- registerMove xs (c, x, y)
+   Adds a move to the to the current board, and places it in the appropriet place in the list. The move is a tuple with the stone, x- and y-coordinates.
+   RETURNS: A sorted board with the new move.
+   EXAMPLES: registerMove [(Position X 2 3), (Position O 2 4)] (X, 3, 1) = [(Position X 3 1),(Position X 2 3),(Position O 2 4)]
+-}
 registerMove :: Board -> (Stone, Integer, Integer) -> Board
 
+-- VARIANT: length xs
 registerMove [] (c, x, y) = [(Position c x y)]
 registerMove xs (c, x, y) = findRow xs (c, x, y)
 
 
---Since the Board list needs to be sorted by y over x (due to the showBoard function) findRow is used before findColumn
+{- findRow  xs (c, x, y)
+   Finds the appropriet row to place the new move.
+   RETURNS: A sorted board with the new move.
+-}
 findRow :: Board -> (Stone, Integer, Integer) -> Board
 
+-- VARIANT: length xs
 findRow [] (c', x', y') = [Position c' x' y']
 findRow ((Position c x y):xs) (c', x', y')
    | y < y' = [Position c x y]++(findRow xs (c', x', y'))
@@ -81,12 +124,119 @@ findRow ((Position c x y):xs) (c', x', y')
    | y == y' = findColumn ((Position c x y):xs) (c', x', y')
 
 
---Finds where to place the new square in the Board list
+{- findColumn xs (c, x, y)
+   Finds the appropriet column to place the new move, and adds it to the list.
+   RETURNS: A sorted board with the new move.
+-}
 findColumn :: Board -> (Stone, Integer, Integer) -> Board
 
+-- VARIANT: length xs
 findColumn [] (c', x', y') = [Position c' x' y']
 findColumn((Position c x y):xs) (c', x', y')
    | x < x' && y == y' = [Position c x y]++(findColumn xs (c', x', y'))
    | x < x' && y > y' = [Position c' x' y']++[Position c x y]++xs
    | x > x' = [Position c' x' y']++[Position c x y]++xs
    | otherwise = [Position c' x' y']++[Position c x y]++xs
+
+
+{- checkWinX xs s x y i
+   Checks the board to see if there is a horizontal victory.
+   PRE:  The integer, i, should not be the number 4 when the function is called for the first time.
+   RETURNS: True if there is a horizontal victory, otherwise False.
+   EXAMPLES: checkWinX [(Position X 1 1),(Position X 2 1),(Position X 3 1),(Position X 4 1),(Position O 2 2),Position O 3 2),(Position O 2 3)] X 0 0 0 = True
+             checkWinX [(Position X 1 1),(Position X 2 1),(Position X 3 1),(Position X 4 1),(Position O 2 2),Position O 3 2),(Position O 2 3)] O 0 0 0 = False
+-}
+checkWinX :: Board -> Stone -> Integer -> Integer -> Integer -> Bool
+
+-- VARIANT: length xs
+checkWinX xs s' x y 4 = True
+checkWinX [] s' x y ix = False
+checkWinX ((Position s c r):xs) s' x y ix
+   | s /= s' = checkWinX xs s' c r 0
+   | y /= r && s == s' = checkWinX xs s' c r 1
+   | y == r && (x + 1) == c = checkWinX xs s' c y (ix + 1)
+   | otherwise = checkWinX xs s' c r 1
+
+
+{- checkWinY xs s i
+   Checks the board to see if there is a vertical victory.
+   PRE:  The integer, i, should not be the number 4 when the function is called for the first time.
+   RETURNS: True if there is a vertical victory, otherwise False
+   EXAMPLES: checkWinY [(Position X 1 1),(Position X 1 2),(Position O 2 2),(Position O 3 2),(Position X 1 3),(Position O 2 3),(Position X 1 4)] X 0 = True
+             checkWinY [(Position X 1 2),(Position O 2 2),(Position O 3 2),(Position X 1 3),(Position O 2 3),(Position X 1 4)] O 0 = False
+-}
+checkWinY :: Board -> Stone -> Integer -> Bool
+
+-- VARIANT: length xs
+checkWinY xs s 4 = True
+checkWinY [] s i = False
+checkWinY ((Position s c r):xs) s' i
+   | s == s' = checkWinY xs s' (checkVertical xs s' c r 1)
+   | otherwise = checkWinY xs s' i
+
+
+{- checkVertical xs s y i
+   Checks each square to see if they make a vertical victory.
+   PRE:  The integer, i, should not be the number 4 when the function is called for the first time.
+   RETURNS: 4 if there is a vertical victory, otherwise 0.
+-}
+checkVertical :: Board -> Stone -> Integer -> Integer -> Integer -> Integer
+
+-- VARIANT: length xs
+checkVertical xs s x y 4 = 4
+checkVertical [] s x y i = 0
+checkVertical ((Position s c r):xs) s' x y i
+   | c == x && s == s' && r == (y + 1) = checkVertical xs s' c r (i + 1)
+   | c == x && s /= s' = checkVertical xs s' c r 0
+   | c == x && s == s = checkVertical xs s' c r 1
+   | otherwise = checkVertical xs s' x y i
+
+
+{- checkWinXY xs s i
+   Checks the board to see if there is a diagonal victory.
+   PRE:  The integer, i, should not be the number 4 when the function is called for the first time.
+   RETURNS: True if there is a diagonal victory, otherwise False.
+   EXAMPLES: checkWinXY [(Position X 3 3),(Position O 3 4),(Position X 4 4),(Position X 5 5),(Position O 6 5),(Position X 6 6)] X 0 = True
+             checkWinXY [(Position X 3 3),(Position O 3 4),(Position X 4 4),(Position X 5 5),(Position O 6 5),(Position X 6 6)] O 0 = False
+-}
+checkWinXY :: Board -> Stone -> Integer -> Bool
+
+-- VARIANT: length xs
+checkWinXY xs s 4 = True
+checkWinXY [] s i = False
+checkWinXY ((Position s c r):xs) s' i
+   | s == s' = checkWinXY xs s' (checkDiagonal xs s c r 1)
+   | otherwise = checkWinXY xs s' i
+
+
+{- checkDiagonal xs s x y i
+   Checks each square to see if they make a diagonal victory.
+   PRE:  The integer, i, should not be the number 4 when the function is called for the first time.
+   RETURNS: 4 if there is a diagonal victory, otherwise 0.
+-}
+checkDiagonal :: Board -> Stone -> Integer -> Integer -> Integer -> Integer
+
+-- VARIANT: length xs
+checkDiagonal xs s x y 4 = 4
+checkDiagonal [] s x y i = 0
+checkDiagonal ((Position s c r):xs) s' x y i
+   | s == s' && c == (x + 1) && r == (y + 1) = checkDiagonal xs s' c r (i + 1)
+   | s /= s' && c == (x + 1) && r == (y + 1) = checkDiagonal xs s' c r 0
+   | s == s' && c == (x - 1) && r == (y + 1) = checkDiagonal xs s' c r (i + 1)
+   | s /= s' && c == (x - 1) && r == (y + 1) = checkDiagonal xs s' c r 0
+   |otherwise = checkDiagonal xs s' x y i
+
+
+
+-- Test cases
+gTest1 = TestCase (assertEqual "registerMove" [(Position X 3 1),(Position X 2 3),(Position O 2 4)] (registerMove [(Position X 2 3), (Position O 2 4)] (X, 3, 1)))
+
+gTest2 = TestCase (assertEqual "checkWinX" False (checkWinX [(Position X 1 1),(Position X 2 1),(Position X 3 1),(Position X 4 1),(Position O 2 2),(Position O 3 2),(Position O 2 3)] O 0 0 0))
+
+gTest3 = TestCase (assertEqual "checkWinY" True (checkWinY [(Position X 1 1),(Position X 1 2),(Position O 2 2),(Position O 3 2),(Position X 1 3),(Position O 2 3),(Position X 1 4)] X 0))
+
+gTest4 = TestCase (assertEqual "checkVertical" 4 (checkVertical [(Position X 1 2),(Position O 2 2),(Position O 3 2),(Position X 1 3),(Position O 2 3),(Position X 1 4)] X 1 1 1))
+
+gTest5 = TestCase (assertEqual "checkWinXY" True (checkWinXY [(Position X 3 3),(Position O 3 4),(Position X 4 4),(Position X 5 5),(Position O 6 5),(Position X 6 6)] X 0))
+
+gTest6 = TestCase (assertEqual "checkDiagonal" 4 (checkDiagonal [(Position O 3 4),(Position X 4 4),(Position X 5 5),(Position O 6 5),(Position X 6 6)] X 3 3 1))
--- TicTacToe/app/Main.hs
+++ TicTacToe/app/Main.hs
@@ -1,48 +1,99 @@
+{-
+   Project: Tic-Tac-Toe
+   Module: Main.hs
+   Authors: Sara Gustavsson and William Sandkvist
+   2018-08-18
+-}
 module Main where
 
 import Game
 import Data.List
+import Test.HUnit
 
 
---The main game loop
+{- play board stone
+   The main game loop of the program. It takes a board and a stone and lets the player of the current stone choose a square
+   to place it in. If there are 4 stones in a vertical, horizontal or diagonal row then the player of said row wins and
+   are returned to the main function. The loop also prints the board and registers the moves.
+   SIDE EFFECTS: Prints strings to the console.
+-}
 play :: Board -> Stone -> IO ()
-play board stone = do
-  Game.showBoard board
-  if stone == X
-    then putStrLn "Player X's turn\n"
-    else putStrLn "Player O's turn\n"
-  putStrLn "Choose a square (e.g. A4, b3, c4)"
-  input <- getLine
-  if not(checkInputX input && checkInputY input)
-    then do
-      putStrLn "Invalid input, please try again"
-      play board stone
-    else do
-      let convertedInput = (stone, (charInput input), (integerInput input))
-      let board2 = Game.registerMove board convertedInput
-      let stone2 = if stone == X
-          then O
-          else X
-      play board2 stone2
 
--- The executable function for the entire game
+-- VARIANT: if (Game.checkWinX board2 stone 0 0 0 || Game.checkWinY board2 stone 0 || Game.checkWinXY board2 stone 0)
+play board stone = do
+   putStrLn "\n \n \n"
+   Game.showBoard board
+   if stone == X
+      then putStrLn "Player X's turn\n"
+      else putStrLn "Player O's turn\n"
+   putStrLn "Choose a square (e.g. A4, b3, c4)"
+   putStrLn "Type Q to return to the main menu."
+   input <- getLine
+   if (input == "Q" || input == "q")
+      then do
+         main
+      else if not(checkInputX input && checkInputY input)
+         then do
+            putStrLn "\n\nInvalid input. Please, try again."
+            play board stone
+         else do
+            let convertedInput = (stone, (charInput input), (integerInput input))
+            if checkDuplicate convertedInput board
+               then do
+                  putStrLn "\n\nThere's already a stone on that square. Please try another one."
+                  play board stone
+               else do
+                  let board2 = Game.registerMove board convertedInput
+                  if (Game.checkWinX board2 stone 0 0 0 || Game.checkWinY board2 stone 0 || Game.checkWinXY board2 stone 0)
+                     then do
+                        Game.showBoard board2
+                        putStrLn "\n\nPlayer "
+                        print stone
+                        putStrLn "Winner!"
+                        main
+                     else if (length board2 == 81)
+                        then do
+                           putStrLn "\n\nGame draw!"
+                           main
+                        else do
+                           let stone2 = if stone == X
+                                          then O
+                                          else X
+                           play board2 stone2
+
+
+{- main
+   The main game function. It asks if a player wants to start a game or quit the game. If the player wants to start a game,
+   then the play function is called upon. If they want to quit then the function returns ().
+   RETURNS: ()
+   SIDE EFFECTS: Prints strings to the console.
+-}
 main:: IO()
+
+-- VARIANT:
 main = do
    putStrLn $ take 2 $ repeat '\n'
    putStrLn "Welcome to our TicTacToe game!\n"
    putStrLn "1) Start a game"
-   putStrLn "2) Quit \n"
+   putStrLn "2) Quit the game \n"
    input <- getLine
    if input == "1"
       then play [] X
       else if input == "2"
-	     then return()
+         then return()
          else do
             putStrLn "Invalid input, please type either '1' or '2'"
 
 
+{- checkInputX xs
+   checks to see if the given move input's x-coordinate is valid.
+   RETURNS: True if the move is valid, otherwise false.
+   EXAMPLES: checkInputX "c4" = True
+             checkInputX "foo" = False
+-}
 checkInputX :: String -> Bool
 
+checkInputX [] = False
 checkInputX [x, _]
    | x == 'A' || x == 'a' = True
    | x == 'B' || x == 'b' = True
@@ -54,10 +105,18 @@ checkInputX [x, _]
    | x == 'H' || x == 'h' = True
    | x == 'I' || x == 'i' = True
    |otherwise = False
+checkInputX xs = False
 
 
+{- checkInputY xs
+   Checks to see if the given move input's y-coordinate-coordinate is valid.
+   RETURNS: True if the move is valid, otherwise False.
+   EXAMPLES: checkInputX "c4" = True
+             checkInputX "foo" = False
+-}
 checkInputY :: String -> Bool
 
+checkInputY [] = False
 checkInputY [_, y]
    | y == '1' = True
    | y == '2' = True
@@ -69,10 +128,18 @@ checkInputY [_, y]
    | y == '8' = True
    | y == '9' = True
    |otherwise = False
+checkInputY xs = False
 
 
---Converts the given two character string to the appropriate x-coordinate
+{- charInput xs
+   Converts the given two character string to the appropriate x-coordinate.
+   PRE:  The string must be of two charaters, where the first one is a letter A-I.
+   RETURNS: An integer based of the letter.
+   EXAMPLES: checkInputX "c4" = 3
+             checkInputX "D3" = 4
+-}
 charInput :: String -> Integer
+
 charInput [x, _]
    | x == 'A' || x == 'a' = 1
    | x == 'B' || x == 'b' = 2
@@ -85,7 +152,13 @@ charInput [x, _]
    | x == 'I' || x == 'i' = 9
 
 
---converts the given two character string to the appropriate y-coordinate
+{- integerInput xs
+   Converts the given two character string to the appropriate y-coordinate.
+   PRE:  The string must be of two charaters, where the second one is a number 1 - 2.
+   RETURNS: The given number as an integer
+   EXAMPLES: checkInputX "c4" = 4
+             checkInputX "D3" = 3
+-}
 integerInput :: [Char] -> Integer
 
 integerInput [_, y]
@@ -98,3 +171,38 @@ integerInput [_, y]
    | y == '7' = 7
    | y == '8' = 8
    | y == '9' = 9
+
+
+{- checkDuplicate s xs
+   Checks if the given input's x- and y-coordinate has already been registered to the board.
+   RETURNS: True if the given input is already in the board, otherwise False.
+   EXAMPLES: checkDuplicate (X, 3, 4) [(Position O 1 1), (Position X 3 4)] = True
+             checkDuplicate (X, 1, 1) [(Position O 1 1), (Position X 3 4)] = True
+             checkDuplicate (X, 1, 2) [(Position O 1 1), (Position X 3 4)] = False
+-}
+checkDuplicate :: (Stone, Integer, Integer) -> Board -> Bool
+
+-- VARIANT: length xs
+checkDuplicate s [] = False
+checkDuplicate (s, x, y) ((Position s' c r):xs)
+   | x == c && y == r = True
+   | otherwise = checkDuplicate (s, x, y) xs
+
+
+{-
+   Test cases
+-}
+mTest1 = TestCase (assertEqual "checkDuplicate" True (checkDuplicate ( X, 6, 5) [(Position O 3 4),(Position X 4 4),(Position X 5 5),(Position O 6 5),(Position X 6 6)]))
+
+mTest2 = TestCase (assertEqual "checkInputX " True (checkInputX "c4"))
+
+mTest3 = TestCase (assertEqual "checkInputY" False (checkInputY "foo"))
+
+mTest4 = TestCase (assertEqual "charInput" 3 (charInput "c4"))
+
+mTest5 = TestCase (assertEqual "integerInput" 8 (integerInput "d8"))
+
+mTest6 = TestCase (assertEqual "checkDuplicate" True (checkDuplicate (X, 1, 1) [(Position O 1 1), (Position X 3 4)]))
+
+
+performTests = runTestTT (TestList [mTest1,mTest2,mTest3,mTest4,mTest5,mTest6,gTest1,gTest2,gTest3,gTest4,gTest5,gTest6])
